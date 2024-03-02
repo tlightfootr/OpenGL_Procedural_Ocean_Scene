2ddf881c800f6f30a951bbd14ee6a754c869d993
tomli <tomlightfootr@gmail.com>
2024-03-02 20:19:54 +1300

    build 3
    blend files ignored
    
    blend files removed
    
    png to jpg
    
    skybox reflections

---
+++ Shaders/Vertex.glsl
@@ -0,0 +1,61 @@
+#version 400 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform float time;
+
+const float e = 2.71828182;
+
+float dirVals[] = float[200](0.1181, 0.7204, 0.399, 0.3315, 0.27, 0.0525, 0.7082, 0.573, 0.6913, 0.6627, 0.8906, 0.6152, 0.5396, 0.6569, 0.0348, 0.1966, 0.0246, 0.9251, 0.2854, 0.4396, 0.3261, 0.6801, 0.6628, 0.1879, 0.549, 0.8352, 0.737, 0.3302, 0.5004, 0.597, 0.8465, 0.6073, 0.8697, 0.4528, 0.5772, 0.5376, 0.5698, 0.3914, 0.4541, 0.1949, 0.1713, 0.5778, 0.0912, 0.4045, 0.9188, 0.5455, 0.4215, 0.4241, 0.4257, 0.6538, 0.1291, 0.3638, 0.3057, 0.1002, 0.5306, 0.1247, 0.5095, 0.0901, 0.1599, 0.7645, 0.4373, 0.3378, 0.5803, 0.7635, 0.029, 0.7913, 0.6635, 0.163, 0.56, 0.3703, 0.5552, 0.4412, 0.6287, 0.8771, 0.5801, 0.113, 0.1902, 0.6362, 0.9159, 0.7758, 0.8096, 0.3629, 0.3983, 0.864, 0.4521, 0.8314, 0.382, 0.8822, 0.4884, 0.4091, 0.4368, 0.7526, 0.8525, 0.4033, 0.0497, 0.4059, 0.3694, 0.2164, 0.9247, 0.9183, 0.5486, 0.5435, 0.7661, 0.0724, 0.6063, 0.4446, 0.1245, 0.7107, 0.4154, 0.2173, 0.5605, 0.1681, 0.1123, 0.1818, 0.0172, 0.436, 0.949, 0.1364, 0.9832, 0.0322, 0.2651, 0.8959, 0.9999, 0.2043, 0.6699, 0.0394, 0.4548, 0.8251, 0.4208, 0.5691, 0.7287, 0.7869, 0.6427, 0.0486, 0.0236, 0.2713, 0.1181, 0.0416, 0.1214, 0.283, 0.9385, 0.9949, 0.3472, 0.5112, 0.9528, 0.7729, 0.5303, 0.8539, 0.4347, 0.1028, 0.6847, 0.6711, 0.5477, 0.9222, 0.6032, 0.2049, 0.9149, 0.6373, 0.8468, 0.7557, 0.8362, 0.2838, 0.8897, 0.7383, 0.0256, 0.8907, 0.9911, 0.8871, 0.2335, 0.5352, 0.6809, 0.3463, 0.9121, 0.1994, 0.2212, 0.538, 0.7594, 0.5238, 0.8832, 0.4659, 0.2247, 0.6241, 0.8734, 0.9455, 0.9748, 0.0537, 0.272, 0.149, 0.65, 0.4057, 0.2471, 0.0017, 0.7419, 0.8441, 0.2318, 0.7489, 0.6465, 0.2379, 0.2845, 0.9995);
+
+const int num = 15;
+
+out vec3 FragPos;
+out vec3 NormalPos;
+out vec2 TexCoords;
+
+float waveHeight(float x, float z);
+float calcWave(vec2 p, int i, float x, float z);
+vec3 waveNormal(float x, float z);
+float calcWaveDx(vec2 p, int i, float x, float z);
+float calcWaveDy(vec2 p, int i, float x, float z);
+
+float lastDx = 0.0;
+float lastDy = 0.0;
+
+float prevDx = 0.0;
+float prevDy = 0.0;
+
+void main()
+{
+    vec3 position = vec3(aPos.x, aPos.y + waveHeight(aPos.x, aPos.z), aPos.z);
+
+    gl_Position = projection * view * model * vec4(position, 1.0);
+    NormalPos = position;
+    FragPos = vec3(model * vec4(position, 1.0));
+    TexCoords = aTexCoords;
+
+}
+
+float waveHeight(float x, float z) {
+    float height = 0.0;
+    for (int i = 0; i < num; i++)
+    {
+        vec2 p = normalize(vec2(dirVals[i], dirVals[i+100]));
+        height += calcWave(p, i, x, z);
+    }
+    return height;
+}
+
+float calcWave(vec2 p, int i, float x, float z)
+{
+    float amplitude = 1 * pow(0.6, i);
+    float frequency = 1 * pow(1.4, i);
+    float ampConst = 0.5;
+    return pow(e, ampConst * amplitude * sin(dot(p, vec2(x, z)) * frequency + time) - 1.0) - 0.5;
+}
---
+++ Source.cpp
@@ -0,0 +1,333 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include <stdlib.h>
+
+#include "shader.h"
+#include "stb_image.h"
+#include "model.h"
+#include "mesh.h"
+
+
+void process_input(GLFWwindow* window, float deltaTime);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+unsigned int loadCubemap(vector<std::string> faces);
+
+float vertices[] = {
+	0.0f, 0.5f, 0.0f,
+	0.0f, 0.0f, 0.0f,
+	0.5f, 0.0f, 0.0f
+};
+
+float skyboxVertices[] = {
+	// positions          
+	-1.0f,  1.0f, -1.0f,
+	-1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+	 1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+
+	-1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
+
+	 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+
+	-1.0f, -1.0f,  1.0f,
+	-1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
+
+	-1.0f,  1.0f, -1.0f,
+	 1.0f,  1.0f, -1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	-1.0f,  1.0f,  1.0f,
+	-1.0f,  1.0f, -1.0f,
+
+	-1.0f, -1.0f, -1.0f,
+	-1.0f, -1.0f,  1.0f,
+	 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+	-1.0f, -1.0f,  1.0f,
+	 1.0f, -1.0f,  1.0f
+};
+
+glm::vec3 pointLightPos = glm::vec3(-3.0f, 2.0f, -1.0f);
+
+// camera setup
+glm::vec3 cameraPos = glm::vec3(0.0f, 3.0f, 0.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float pitch = 0.0f, yaw = -90.0f;
+float lastX = 400, lastY = 300;
+
+bool firstMouse = true;
+
+// delta time
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
+int main() {
+
+	glfwInit();
+
+	//set openGL version to 3.3 CORE
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//create window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLProject", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "failed to initialise GLFW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	//glfw configs
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+
+	//init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	stbi_set_flip_vertically_on_load(true);
+	// create shader objects
+	Shader objectShader = Shader("Shaders/Vertex.glsl", "Shaders/Fragment.glsl");
+	Shader lightShader = Shader("Shaders/VertexLight.glsl", "Shaders/FragmentLight.glsl");
+	Shader skyBoxShader = Shader("Shaders/VertexSkyBox.glsl", "Shaders/FragmentSkyBox.glsl");
+	objectShader.use();
+
+	unsigned int cubeMapVBO;
+	glGenBuffers(1, &cubeMapVBO);
+
+	unsigned int cubeMapVAO;
+	glGenVertexArrays(1, &cubeMapVAO);
+
+	// ..:: Initialization code :: ..
+	// 1. bind Vertex Array Object
+	glBindVertexArray(cubeMapVAO);
+	// 2. copy our vertices array in a vertex buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, cubeMapVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
+	// 4. then set the vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// cube map
+	vector<std::string> textures_faces
+	{
+		"textures/skybox/right.jpg",
+		"textures/skybox/left.jpg",
+		"textures/skybox/top.jpg",
+		"textures/skybox/bottom.jpg",
+		"textures/skybox/front.jpg",
+		"textures/skybox/back.jpg"
+	};
+	stbi_set_flip_vertically_on_load(false);
+	unsigned int cubemapTexture = loadCubemap(textures_faces);
+	stbi_set_flip_vertically_on_load(true);
+
+	// material values
+	objectShader.setInt("material.diffuse", 0);
+	objectShader.setInt("material.specular", 1);
+	objectShader.setFloat("material.shininess", 64.0f);
+
+	objectShader.setVec3("pointLight.position", pointLightPos.x, pointLightPos.y, pointLightPos.z);
+	objectShader.setVec3("pointLight.ambient", 0.1f, 0.1f, 0.1f);
+	objectShader.setVec3("pointLight.diffuse", 0.1f, 0.1f, 0.1f);
+	objectShader.setVec3("pointLight.specular", 1.0f, 1.0f, 0.8f);
+	objectShader.setFloat("pointLight.constant", 1.0f);
+	objectShader.setFloat("pointLight.linear", 0.09f);
+	objectShader.setFloat("pointLight.quadratic", 0.032f);
+
+	objectShader.setVec3("viewPos", cameraPos.x, cameraPos.y, cameraPos.z);
+
+	// enable depth testing
+	glEnable(GL_DEPTH_TEST);
+
+	Model water = Model("Models/water2/water.obj");
+	Model light = Model("Models/cube/cube.obj");
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// -------------------- Main Loop -------------------- //
+	while (!glfwWindowShouldClose(window))
+	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		//process input
+		process_input(window, deltaTime);
+
+		//render
+		// clear screen 
+		glEnable(GL_DEPTH_TEST); 
+		glDepthFunc(GL_LEQUAL);
+		glClearColor(0.2f, 0.4f, 0.6f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		glm::mat4 projection = glm::perspective(glm::radians(90.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
+
+		objectShader.use();
+		// update uniforms per frame
+		objectShader.setVec3("viewPos", cameraPos.x, cameraPos.y, cameraPos.z);
+		objectShader.setFloat("time", glfwGetTime());
+
+		// draw water
+		glm::mat4 model = glm::mat4(1.0f);
+		objectShader.setMat4("model", model);
+		objectShader.setMat4("view", view);
+		objectShader.setMat4("projection", projection);
+		water.Draw(objectShader);
+
+		//draw light
+		lightShader.use();
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, pointLightPos);
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+		lightShader.setMat4("model", model);
+		lightShader.setMat4("view", view);
+		lightShader.setMat4("projection", projection);
+		//light.Draw(lightShader);
+
+		glDepthMask(GL_FALSE);
+		skyBoxShader.use();
+
+		glm::mat4 skyBoxView = glm::mat4(glm::mat3(view));
+		skyBoxShader.setMat4("view", skyBoxView);
+		skyBoxShader.setMat4("projection", projection);
+
+		glBindVertexArray(cubeMapVAO);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glDepthMask(GL_TRUE);
+
+		// check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	// -------------------- Loop End -------------------- //
+
+	// end program
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
+void process_input(GLFWwindow* window, float deltaTime)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	const float cameraSpeed = 8.0f * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		cameraPos.y += cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+		cameraPos.y -= cameraSpeed;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouse) // initially set to true
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates range from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	const float sensitivity = 0.1f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(direction);
+}
+
+unsigned int loadCubemap(vector<std::string> faces)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+	int width, height, nrChannels;
+	for (unsigned int i = 0; i < faces.size(); i++)
+	{
+		unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	return textureID;
+}
+
